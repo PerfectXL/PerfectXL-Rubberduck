10491031441963a233f275939384fc6804700589
Roel van der Steen <roel@st2x.net>
2017-07-11 12:22:22 +0200

    Modify project to make it work with the "next" branch of Rubberduck
    We want the newest developments.

--- PerfectXL.VbaCodeAnalyzer/Models/AttributeParser.cs
+++ PerfectXL.VbaCodeAnalyzer/Models/AttributeParser.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Threading;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
 using Rubberduck.Parsing.Symbols;
 using Rubberduck.Parsing.VBA;
 using Rubberduck.VBEditor.SafeComWrappers.Abstract;
@@ -9,8 +11,11 @@ namespace PerfectXL.VbaCodeAnalyzer.Models
 {
     internal class AttributeParser : IAttributeParser
     {
-        public IDictionary<Tuple<string, DeclarationType>, Attributes> Parse(IVBComponent component, CancellationToken token)
+        public IDictionary<Tuple<string, DeclarationType>, Attributes> Parse(IVBComponent component, CancellationToken token, out ITokenStream stream,
+            out IParseTree tree)
         {
+            stream = null;
+            tree = null;
             return new Dictionary<Tuple<string, DeclarationType>, Attributes>();
         }
     }
