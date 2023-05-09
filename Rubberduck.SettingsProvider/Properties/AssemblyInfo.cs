ebf995a8e5f516469bf4f2a3785c9773fa758e97
Willem-Jan Meerkerk <willemjan@perfectxl.com>
2023-05-09 14:04:49 +0200

    Add project Rubberduck.SettingsProvider
    Files are from Rubberduck-Prerelease-v2.5.2.6260

---
+++ Rubberduck.SettingsProvider/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+
+//This is the assembly name of the project Rubberduck.Main.
+[assembly: InternalsVisibleTo("Rubberduck")]
+[assembly: InternalsVisibleTo("RubberduckTests")]
+// Moq needs this to access the types
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+
+[assembly: ComVisible(false)]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("26096055-4801-45f6-a82b-add86311eddf")]
