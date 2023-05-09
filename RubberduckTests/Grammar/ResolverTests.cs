174c2c7d70178a723c4900d75c8e90ac0e72fad8
Willem-Jan Meerkerk <willemjan@perfectxl.com>
2023-05-09 16:56:53 +0200

    Modifications for build of RubberduckTests

--- RubberduckTests/Grammar/ResolverTests.cs
+++ RubberduckTests/Grammar/ResolverTests.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using NUnit.Framework;
-using Rubberduck.InternalApi.Extensions;
 using Rubberduck.Parsing.Symbols;
 using Rubberduck.Parsing.VBA;
 using RubberduckTests.Mocks;
--- RubberduckTests/ParserState/ParserStateTests.cs
+++ RubberduckTests/ParserState/ParserStateTests.cs
@@ -20,8 +20,8 @@ namespace RubberduckTests.ParserStateTests
         public void SetUp()
         {
             // Replace pop-up assert trace listener with one that simply logs a message.
-            Debug.Listeners.Clear();
-            Debug.Listeners.Add(new ConsoleTraceListener());
+            Trace.Listeners.Clear();
+            Trace.Listeners.Add(new ConsoleTraceListener());
         }
 
         [Test]
